995fcce2a41eeb6aa1233163531e041cae84468c
sitanshmehta <s65mehta@uwaterloo.ca>
2024-11-17 21:04:16 -0500

    square class setup

---
+++ square.cpp
@@ -0,0 +1,37 @@
+#include "square.h"
+#include <QPainter>
+
+Square::Square(int x, int y, QWidget *parent)
+    : QWidget(parent), x(x), y(y), piece(nullptr)
+{
+    setFixedSize(50, 50);
+}
+
+int Square::getX() const {
+    return x;
+}
+
+int Square::getY() const {
+    return y;
+}
+
+Piece* Square::getPiece() const {
+    return piece;
+}
+
+void Square::setPiece(Piece *newPiece) {
+    piece = newPiece;
+    update();
+}
+
+void Square::paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
+
+    QColor color = ((x+y) % 2 == 0) ? QColor(Qt::white) : QColor(Qt::black);
+    painter.fillRect(rect(), color);
+
+    if(piece) {
+        //QPixmap pixmap = piece->getPixmap();
+        //painter.drawPixmap(rect(), pixmap);
+    }
+}
---
+++ square.h
@@ -0,0 +1,33 @@
+#ifndef SQUARE_H
+#define SQUARE_H
+
+#include <QObject>
+#include <QWidget>
+
+class Piece;
+
+//Each square will handle mouse events and display graphical content, therefore it inherits QWidget
+class Square : public QWidget
+{
+    //For signals and slots in the square class
+    Q_OBJECT
+
+public:
+    Square(int x, int y, QWidget *parent = nullptr);
+
+    int getX() const;
+    int getY() const;
+    Piece* getPiece() const;
+    void setPiece(Piece* newPiece);
+
+//override default behavior of paintEvent function that paints widgets
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    int x;
+    int y;
+    Piece *piece;
+};
+
+#endif // SQUARE_H
