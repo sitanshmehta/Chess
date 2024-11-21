90715f3d14897f317a490b6a73e50bbf1a8fd2e1
sitanshmehta <s65mehta@uwaterloo.ca>
2024-11-21 07:58:13 -0500

    initialising rook placement rook on board

--- rook.cpp
+++ rook.cpp
@@ -6,9 +6,9 @@ Rook::Rook(Color color)
     : Piece(color)
 {
     if (color == WHITE) {
-        icon = QPixmap(":/images/rook_white.png");
+        icon = QPixmap(":/images/images/rook_white.png");
     } else {
-        icon = QPixmap(":/images/rook_black.png");
+        icon = QPixmap(":/images/images/rook_black.png");
     }
 }
 
