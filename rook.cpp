81d47321665dd7ad19cc4251163ad74e6c221a46
sitanshmehta <s65mehta@uwaterloo.ca>
2024-11-23 22:39:21 -0500

    valid moves for rook

--- rook.cpp
+++ rook.cpp
@@ -24,14 +24,31 @@ std::vector<Square*> Rook::getValidMoves(const Board& board) const
     std::vector<Square*> validMoves;
 
     Square* currSquare = this->getCurrSquare();
+    Piece* currPiece = currSquare->getPiece();
+    Color currPieceColor = currPiece->getColor();
+
     int x = currSquare->getX();
     int y = currSquare->getY();
 
-    for(int i = x; i < 8; i++) {
-        Square* square = board.getSquare(x, i);
-        if(square->getPiece() != nullptr) {
+    qDebug() << "x: " << x << "y" << y;
+    //Up
+    for(int i = x; i < 8; i--) {
+        Square* square = board.getSquare(x + 1, i);
+
+        qDebug() << "X: " << square->getX() << "Y" << square->getY();
+
+        qDebug() << i << " " << x;
+        //qDebug() <<square->getX() << " "<< square->getY();
+        if(square->getPiece() == nullptr) {
+            //qDebug() << "here";
+            validMoves.push_back(square);
+            continue;
+        } else {
             break;
         }
+        //if(currPiece != nullptr && square->getPiece()->getColor() != currPieceColor){
+            //validMoves.push_back(square);
+        //}
         validMoves.push_back(square);
     }
     return validMoves;
